b5befd822f9dd32cdd635011be084a62f3f28cad
Heather Baldwin <hbaldwin@Heathers-MacBook-Pro.local>
2020-07-26 22:15:45 -0400

    fixed tests - was using wrong channel assignment for test image

--- tests/testthat/test-image_segmentation.R
+++ tests/testthat/test-image_segmentation.R
@@ -2,7 +2,7 @@
 
 test_that("Nuclei segmentation returns >3 mask objects", {
   img = LoadImage("~/Documents/test_tifs/HAB135_WT_Tom20-CAT-dapi_tile-1.czi_-_WT_Tom20-CAT-dapi_tile-1_02.tif")
-  nseg = segmentNucleus(img, index=1, minmaxnorm=TRUE,
+  nseg = segmentNucleus(img, index=3, minmaxnorm=TRUE,
                             int=2, filter_size=25, offset=0.01, opensize=3,
                             small_obj=30, use_watershed=FALSE, distmap_value=2,
                             rm_outliers=FALSE, out_p=0.95, displaymasks=TRUE)
@@ -12,7 +12,7 @@ test_that("Nuclei segmentation returns >3 mask objects", {
 
 test_that("Nuclei segmentation-remove outliers", {
   img = LoadImage("~/Documents/test_tifs/HAB135_WT_Tom20-CAT-dapi_tile-1.czi_-_WT_Tom20-CAT-dapi_tile-1_02.tif")
-  nseg = segmentNucleus(img, image, index=1, minmaxnorm=TRUE,
+  nseg = segmentNucleus(img, index=3, minmaxnorm=TRUE,
                             int=2, filter_size=25, offset=0.01, opensize=3,
                             small_obj=30, use_watershed=FALSE, distmap_value=2,
                             rm_outliers=TRUE, out_p=0.95)
@@ -26,9 +26,8 @@ test_that("Cell segmentation returns >3 mask objects", {
                         int=2, filter_size=25, offset=0.01, opensize=3,
                         small_obj=30, use_watershed=FALSE, distmap_value=2,
                         rm_outliers=FALSE, out_p=0.95, displaymasks=TRUE)
-  cseg = segmentCyto(img, nseg$seg, index=2, int=40, filter_size=10,
-                     offset=0.1, size_smooth=19,
-                     opensize=7, largeobj=30000, minmaxnorm=TRUE)
+  cseg = segmentCyto(img, nseg$seg, index=3, int=10, filter_size=100, 
+                     offset=0.01, size_smooth=19, opensize=7, largeobj=100000, minmaxnorm=TRUE)
   cf = EBImage::computeFeatures.moment(cseg$seg)
   expect(length(cf)>3)
 })
