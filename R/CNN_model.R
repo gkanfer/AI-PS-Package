91c71cfb71c0f149c4d7d9b521dcc979190112bd
Heather Baldwin <hbaldwin@Heathers-MacBook-Pro.local>
2020-07-28 16:27:57 -0400

    simplified and made new function for copying cells to testing, training directories

--- R/CNN_model.R
+++ R/CNN_model.R
@@ -1,95 +1,66 @@
 ## 3 CNN Training
+
+#! Should CNN training have its own pickCells (classification-based selection) function or share the same one from the SVM section? 
+
 #' 3.1 Generation and upload of single cell images for CNN training
-#' @param y Cell segmentation mask generated from segmentCyto
 #' @param x Image 
-#' @param 
+#' @param y Cell segmentation mask generated from segmentCyto
+#' @param pheno0 String. Name of control (normal) phenotype
+#' @param pheno1 String. Name of positive phenotype
 #' @return 
 #' @export
-genImageSet <- function(y, x, base_dir, train_dir, validation_dir, test_dir, train_norm_dir, train_swol_dir, validation_norm_dir, original_dataset_dir_norm, original_dataset_dir_swol, tr.mx, val.mx, tst.mx){
+genImageSet <- function(x, y, base_dir, pheno0, pheno1){
   #Break the 20x magnification image into single cell images
-  stack<-stackObjects(y,x*intens,ext = c(satck_size,satck_size))
-  #Folders creation 
-  train_dir <- createdir(base_dir, "train")
-  val_dir <- file.path(base_dir, "val")
-  dir.create(train_dir)
-  validation_dir <- file.path(base_dir, "validation")
-  dir.create(validation_dir)
-  test_dir <- file.path(base_dir, "test")
-  dir.create(test_dir)
-  train_norm_dir <- file.path(train_dir, "Norm")
-  dir.create(train_norm_dir)
-  train_swol_dir <- file.path(train_dir, "Swol")
-  dir.create(train_swol_dir)
-  validation_norm_dir <- file.path(validation_dir, "Norm")
-  dir.create(validation_norm_dir)
-  validation_swol_dir <- file.path(validation_dir, "Swol")
-  dir.create(validation_swol_dir)
-  test_swol_dir <- file.path(test_dir, "Swol")
-  dir.create(test_swol_dir)
-  test_norm_dir <- file.path(test_dir, "Norm")
-  dir.create(test_norm_dir)
-  # Transfer all images to the apropriete destenation
-  setwd(original_dataset_dir_norm)
-  fnames <- dir()[grep(".tif",dir())]
-  fnames = sample(fnames)
-  length(fnames)
-  file.copy(file.path(original_dataset_dir_norm, fnames[1:(tr.mx+100)]),
-            file.path(train_norm_dir))
-  file.copy(file.path(original_dataset_dir_norm, fnames[(tr.mx+100+1):val.mx+200]),
-            file.path(validation_norm_dir))
-  file.copy(file.path(original_dataset_dir_norm, fnames[(val.mx+200+1):tst.mx+200]),
-            file.path(test_norm_dir))
-  setwd(original_dataset_dir_swol)
-  fnames <- dir()[grep(".tif",dir())]
-  fnames = sample(fnames)
-  length(fnames)
-  file.copy(file.path(original_dataset_dir_swol, fnames[1:tr.mx]),
-            file.path(train_swol_dir))
-  file.copy(file.path(original_dataset_dir_swol, fnames[(tr.mx+1):val.mx]),
-            file.path(validation_swol_dir))
-  file.copy(file.path(original_dataset_dir_swol, fnames[(val.mx+1):(tst.mx)]),
-            file.path(test_swol_dir))
+  stack<-EBImage::stackObjects(y, x*intens,ext = c(satck_size, satck_size)) #! do we need the ext option? Where does satck_size come from?
   
-  Fimg <- function(x) {
-    x1<-readImage(x, type="tiff")
-    dx <- (dim(x1)[1])
-    dx1 <- dx%/%8
-    ft <- matrix(c(-0.05,-0.02,0, -0.02, -0.05), nrow=5, ncol=5)
-    ft[5,5] <- 2
-    y1 <- filter2(x1, ft)
-    y2 <- y1[(dx1-10):(dx-dx1),(dx1-10):(dx-dx1)]
-    writeImage(y2, paste(x),type = "tiff", bits.per.sample = 8L, compression = "LZW")
+  #Save individual files in phenotype dir
+  
+  
+  #! I don't think we should include this preprocessing function. Move to generator creation if we still want that option
+  # Fimg <- function(x) {
+  #   x1<-readImage(x, type="tiff")
+  #   dx <- (dim(x1)[1])
+  #   dx1 <- dx%/%8
+  #   ft <- matrix(c(-0.05,-0.02,0, -0.02, -0.05), nrow=5, ncol=5)
+  #   ft[5,5] <- 2
+  #   y1 <- filter2(x1, ft)
+  #   y2 <- y1[(dx1-10):(dx-dx1),(dx1-10):(dx-dx1)]
+  #   writeImage(y2, paste(x),type = "tiff", bits.per.sample = 8L, compression = "LZW")
+  # }
+  
+}
+
+#' 3.2 Create testing, training directories (split based off of user-inputed ratio) and move files to appropriate destinations
+#' Create files into testing and training sets, create test directory. Remaining files in original base_dir=train_dir 
+#' @param base_dir 
+#' @param pheno0 String. Name of control (normal) phenotype
+#' @param pheno1 String. Name of positive phenotype
+#' @param test_ratio Number between 0-1. Proportion of images to relocate to testing directory
+#' @export
+splitDirs <- function(base_dir, pheno0, pheno1, test_ratio) {
+  cls_list = c(pheno0, pheno1)
+  for (cls in cls_list) {
+    createdir(file.path(base_dir,'test',cls))
+    createdir(file.path(base_dir,'train',cls))
+    src = file.path(base_dir, cls) 
+    allfiles = list.files(path=src, full.names=TRUE)
+    allfiles = sample(allfiles)  #random shuffling 
+    test_n= as.integer(length(allfiles)*test_ratio)
+    test_files=allfiles[0:test_n]
+    train_files=allfiles[test_n:as.integer(length(allfiles))]
+    print(paste0("Testing-",cls,": ",length(test_files)))
+    print(paste0("Training-",cls,": ",length(train_files)))
   }
-  
-  setwd(train_dir)
-  setwd(paste(getwd(),"Swol", sep="/"))
-  #dir()
-  tifsS<-dir()[grep(".tif",dir())]
-  lapply(tifsS, Fimg)
-  
-  setwd(train_dir)
-  setwd(paste(getwd(),"Norm",sep="/"))
-  #dir()
-  tifsS<-dir()[grep(".tif",dir())]
-  lapply(tifsS, Fimg)
-  
-  setwd(validation_dir)
-  setwd(paste(getwd(),"Swol",sep="/"))
-  #dir()
-  tifsS<-dir()[grep(".tif$",dir())]
-  lapply(tifsS, Fimg)
-  
-  setwd(validation_dir)
-  setwd(paste(getwd(),"Norm",sep="/"))
-  #dir()
-  tifsS<-dir()[grep(".tif$",dir())]
-  lapply(tifsS, Fimg)
+  for (i in test_files) {
+    path = file.path(base_dir,"test",cls) 
+    file.copy(i, path)
+  }
 }
 
 ##Internal Functions##
-#' Directory create
-createdir <- function(path, subpath) {
-  dir <- file.path(path, subpath)
-  dir.create(dir)
-  return(dir)
-}
+#' #' Directory create
+#' createdir <- function(path, subpath) {
+#'   dir <- file.path(path, subpath)
+#'   dir.create(dir)
+#'   return(dir)
+#' }
